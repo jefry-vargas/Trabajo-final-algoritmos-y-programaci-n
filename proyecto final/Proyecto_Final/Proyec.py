611aa2b1f50c6eb2e2398739a46c11cca20d4ece
jefry-vargas <102638203+jefry-vargas@users.noreply.github.com>
2022-05-24 19:38:17 -0500

    Update Proyec.py

--- proyecto final/Proyecto_Final/Proyec.py
+++ proyecto final/Proyecto_Final/Proyec.py
@@ -1,9 +1,7 @@
-import pygame, random
+import pygame, random, sys
 pygame.init()
-pygame.mixer.init()
-laser_sonido=pygame.mixer.Sound('sonido/laser.wav')
-explosion_sonido=pygame.mixer.Sound('sonido/explosion.wav')
-golpe_sonido=pygame.mixer.Sound('sonido/golpe.wav')
+
+#constantes
 ANCHO = 1000
 ALTO = 600
 BLACK = (0, 0, 0)
@@ -11,13 +9,18 @@ WHITE = (255, 255, 255)
 
 screen = pygame.display.set_mode([ANCHO, ALTO])
 clock = pygame.time.Clock()
+game_over = False
 ejecutar = False
-
+done = True
 fontlives = pygame.font.SysFont("Algerian", 25)
 fontscore = pygame.font.SysFont("Algerian", 25)
 
 score = 0
 lives = 3
+#sonidos
+laser_sonido=pygame.mixer.Sound('sonido/laser.wav')
+explosion_sonido=pygame.mixer.Sound('sonido/explosion.wav')
+golpe_sonido=pygame.mixer.Sound('sonido/golpe.wav')
 
 #Listas
 all_sprite_list = pygame.sprite.Group()
@@ -27,7 +30,11 @@ laser_list = pygame.sprite.Group()
 
 #Fondo
 fondo = pygame.image.load("Fondos/fondo.jpg").convert()
-
+fondo = pygame.transform.scale(fondo, (ANCHO,ALTO))
+fondo_y = 0
+icono = pygame.image.load("Fondos/icono.png")
+pygame.display.set_icon(icono)
+pygame.display.set_caption("Trabajo final Algoritmos y programación")
 #Clase del jugador
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -56,7 +63,8 @@ class Player(pygame.sprite.Sprite):
 class Enemies(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Enemigos/Green_1.png").convert()
+        green = self.image = pygame.image.load("Enemigos/Green_1.png").convert()
+        green = self.image = pygame.transform.scale(green, [46,44])
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = 20
@@ -75,8 +83,9 @@ class Enemies(pygame.sprite.Sprite):
 class Enemies2(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Enemigos/enemies1.png").convert()
-        self.image.set_colorkey(WHITE)
+        bee = self.image = pygame.image.load("Enemigos/Bee.png").convert()
+        bee = self.image = pygame.transform.scale(bee, [41,39])
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = 890
         self.rect.y = 100
@@ -101,14 +110,14 @@ class Laser(pygame.sprite.Sprite):
     #Movimiento del laser
     def update(self):
         self.rect.y -= 20
-
-#Genera enemigos
+#Genera enemigosl
 contador = 0
 for i in range (5):
     enemies = Enemies()
     enemies.rect.x += contador
     contador+= 50
     enemies_list.add(enemies)
+    all_sprite_list.add(enemies)
 
 contador2 = 0
 for i in range (5):
@@ -116,22 +125,23 @@ for i in range (5):
     enemies2.rect.x += contador2
     contador2-= 50
     enemies_list.add(enemies2)
+    all_sprite_list.add(enemies2)
     
 #Mete al lugador en todos los sprites
 player = Player()
 player_list.add(player)
+all_sprite_list.add(player)
+
+
 
 #Bucle principal
 while not ejecutar:
-    if (len(enemies_list) == 0 or len(player_list) == 0):
-        ejecutar = True
-
     for event in pygame.event.get():
         if  event.type == pygame.QUIT:
-            ejecutar = True
+            sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                ejecutar = True
+                sys.exit()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
@@ -142,7 +152,8 @@ while not ejecutar:
                 laser = Laser()
                 laser.rect.x = player.rect.x + 28
                 laser.rect.y = player.rect.y - 20
-                laser_sonido.play()
+                if not game_over:
+                    laser_sonido.play()
                 #Mete los laser en una lista
                 all_sprite_list.add(laser)
                 laser_list.add(laser)
@@ -153,6 +164,12 @@ while not ejecutar:
                 player.changespeed(3)
             if event.key == pygame.K_d:
                 player.changespeed(-3)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                if game_over:
+                    done = True
+                    
+
 
     #Colision Laser-Enemigos
     for laser in laser_list:
@@ -178,23 +195,29 @@ while not ejecutar:
             if (lives == 0):
                 player_list.remove(player)
 
-    #Dibujar objetos
-    textlives = fontlives.render("Lives: " + str(lives), True, WHITE)
-    textscore = fontscore.render("Point: " + str(score), True, WHITE)
-
-    all_sprite_list.update()
-    player_list.update()
-    enemies_list.update()
-    pygame.display.update()
-
-    screen.blit(fondo, [0,-20])
-    screen.blit (textlives, (20, 520))
-    screen.blit (textscore, (20, 550))
-
-    all_sprite_list.draw(screen)
-    enemies_list.draw(screen)
-    player_list.draw(screen)
-
+    #Dibujar objeto
+    if done:
+        all_sprite_list.update()
+        if (len(enemies_list) == 0 or len(player_list) == 0):
+            game_over = True
+            done = False
+        movimiento_fondo = fondo_y % fondo.get_rect().height
+        screen.blit(fondo, (0,movimiento_fondo-fondo.get_rect().height))
+        if movimiento_fondo < ALTO:
+            screen.blit(fondo, (0,movimiento_fondo))
+        fondo_y += 1
+        textlives = fontlives.render("Lives: " + str(lives), True, WHITE)
+        textscore = fontscore.render("Point: " + str(score), True, WHITE)
+        screen.blit (textlives, (20, 520))
+        screen.blit (textscore, (20, 550))
+        all_sprite_list.draw(screen)
     pygame.display.flip()
+    if not done:
+        font = pygame.font.SysFont("serif", 25)
+        text = font.render("Game over, Click 'R' to play again ", True, WHITE)
+        center_text_x = ANCHO//2
+        center_text_y = ALTO//2
+        screen.blit(fondo, [0,0])
+        screen.blit(text,[center_text_x-170,center_text_y-20])
     clock.tick(60)
 pygame.quit()
